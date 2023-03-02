1b81c64faa01fab1dc127d5c17edf263e51f9f66
AkJoh <jahongirakramov40@gmail.com>
2023-03-02 23:18:34 +0500

    ok

--- app/src/main/java/com/example/a5/adapter/Adapter.kt
+++ app/src/main/java/com/example/a5/adapter/Adapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a5.R
 import com.example.a5.model.Members
@@ -13,7 +12,7 @@ import com.example.a5.model.Members
 class Adapter(private val context: Context, private val memberS: ArrayList<Members>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
     private fun init(holder: RV_ViewHolder, memberS: Members) {
-        holder.recycler.layoutManager = LinearLayoutManager(context,RecyclerView.HORIZONTAL,false)
+        holder.recycler.layoutManager = GridLayoutManager(context,1)
         holder.recycler.adapter = SubAdapter(memberS.members!!)
     }
 
